374e69a0fc83df6ae3a0538c9e1980c3ac5d1507
Ward Muylaert <ward.muylaert@gmail.com>
2010-03-23 12:14:48 +0100

    Creation of admin folder (pages there will be used to LOOK UP IPs)

---
+++ admin/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <title>The Titans - IP Lookup</title>
+    <meta http-equiv="Content-Type" content="application/xhtml+xml;charset=utf-8" />
+    <meta name="author" content="Ward Muylaert" />
+  </head>
+  <body>
+    <div id="page">
+      <div id="menu">
+        <ul>
+          <li>Name</li>
+          <li>ID</li>
+          <li>IP</li>
+          <li>All</li>
+        </ul>
+      </div>
+      <div id="body">
+        This content will change depending on the page we're on
+      </div>
+    </div>
+  </body>
+</html>
