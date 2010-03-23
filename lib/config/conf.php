f3d3628ed9fc1d7d1c929ef1c74c711799471beb
Ward Muylaert <ward.muylaert@gmail.com>
2010-03-23 01:07:15 +0100

    First commit, begin draft of libs

---
+++ lib/config/conf.php
@@ -0,0 +1,20 @@
+<?php
+
+$CONF = array()
+
+$CONF['title'] = "TT IP Logging";
+$CONF['version'] = "2.0 prerelease";
+$CONF['last_update'] = "2010-03-22";
+
+$CONF['dbhost'] = "localhost";
+$CONF['dbname'] = "";
+$CONF['dbuser'] = "";
+$CONF['dbpass'] = "";
+
+/**
+ * timeout is in minutes
+ */
+$CONF['cookie_timeout'] = 15;
+$CONF['cookie_name'] = "ttip";
+
+?>
