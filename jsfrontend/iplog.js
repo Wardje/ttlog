ac27687551b3e04924b80499f10feaf0f1012c3d
Ward Muylaert <ward.muylaert@gmail.com>
2010-03-25 22:57:29 +0100

    A typo had snuck into the javascript frontend, causing IE6 and IE7 to crash as well as making the code not work for Opera. Removed dependency on jQuery and jQuery cookie plugin. Rewritten the way getId() works in light of member_id cookie being set to "0" when the user chose to not be remembered. Rewritten loggedIn() to use getId() since locating of the ID means we can gather all the needed information to make a log.

--- jsfrontend/iplog.js
+++ jsfrontend/iplog.js
@@ -1,5 +1,5 @@
 function loggedIn() {
-  return ($.cookie('member_id') != null && $.cookie('member_id') != "0");
+  return (getId() != null);
 }
 function getNick() {
   var userlinks = document.getElementById("userlinks");
@@ -7,27 +7,60 @@ function getNick() {
   return out[1];
 }
 function getId() {
-  return $.cookie('member_id');
+  var idcookie = getCookie('member_id');
+  if (idcookie != null && idcookie != "0") {
+    return idcookie;
+  }
+  else {
+    var userlinks = document.getElementById("userlinks");
+    var out = userlinks.innerHTML.match(/Logged in as.+?showuser=(\d+)/i);
+    if (out && out.length >= 2) {
+      return out[1];
+    }
+    else {
+      return null;
+    }
+  }
 }
 function logUrl() {
-  return "URL HERE@@@@";
+  return "http://t.wardje.eu/ip/";
 }
 function createFrame(nick, id) {
-  var frame = document.createElement('frame');
-  frame.width = 0;
-  frame.height = 0;
+  var frame = document.createElement('iframe');
+  frame.style.width = 0;
+  frame.style.height = 0;
   frame.frameBorder = 0;
   frame.src = logUrl() + "?name=" + escape(nick) + "&id=" + id;
   frame.style.display = "none";
   return frame;
 }
-function setTtCookie(nick) {
-  var date = new Date();
-  // 20 minutes
-  date.setTime(date.getTime() + (20 * 60 * 1000));
-  $.cookie("ttip", nick, { path: '/', expires: date });
+// get- & setCookie made by w3schools
+function setCookie(cName, value, expireMinutes) {
+  var expiredate = new Date();
+  expiredate.setMinutes(expiredate.getMinutes() + expireMinutes);
+  document.cookie = cName + "=" + escape(value) + ((expireMinutes==null) ? "" : ";expires=" + expiredate.toGMTString());
 }
-if (loggedIn() && $.cookie("ttip") != getNick()) {
-  $("#gfooter").append(createFrame(getNick(), getId()));
+function getCookie(c_name) {
+  if (document.cookie.length > 0) {
+    c_start = document.cookie.indexOf(c_name + "=");
+    if (c_start != -1) { 
+      c_start=c_start + c_name.length+1; 
+      c_end=document.cookie.indexOf(";",c_start);
+      if (c_end == -1)
+          c_end=document.cookie.length;
+      return unescape(document.cookie.substring(c_start,c_end));
+    } 
+  }
+  return null;
+}
+
+function setTtCookie(value) {
+  setCookie("ttip", value, 20);
+}
+function isTtCookie(value) {
+  return (getCookie("ttip") == value);
+}
+if (loggedIn() && isTtCookie()) {
+  document.getElementById("gfooter").appendChild(createFrame(getNick(), getId()));
   setTtCookie(getNick());
 }
