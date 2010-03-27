113396c104d1839f416ea4e4b8af0a211b75acbc
Ward Muylaert <ward.muylaert@gmail.com>
2010-03-27 17:46:01 +0100

    Made admin/ pages XHTML 1.0 STRICT valid. Moved table generating to adminutils.php.

--- lib/adminutils.php
+++ lib/adminutils.php
@@ -22,4 +22,21 @@
   function makeValidId($id) {
     return intval($id);
   }
+
+  function showResultTable($resultArray) {
+    echo "<table>
+  <tr>
+    <th>User</th>
+    <th>IP</th>
+    <th>Time</th>
+  </tr>\n";
+    foreach ($resultArray as $row) {
+      echo "<tr>\n";
+      echo "<td><a href='?p=id&amp;q={$row['id']}'>{$row['name']}</a></td>\n";
+      echo "<td><a href='?p=ip&amp;q={$row['ip']}'>{$row['ip']}</a></td>\n";
+      echo "<td>{$row['time']}</td>\n";
+      echo "</tr>\n";
+    }
+    echo "</table>";
+  }
 ?>
