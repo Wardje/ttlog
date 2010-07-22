1ec2ef1b152429ecb46d403f8a898c9da73d7c54
Ward Muylaert <ward.muylaert@gmail.com>
2010-07-21 21:32:10 -0400

    JavaScript frontend changed to zetaboards
    Due to the sudden disappearance of IPBFree (darnit!), the javascript frontend had to be changed to fit the new HTML layout.
    Note that instead of providing things, it now relies on a "utils" object that has the methods loggedin(), username() and userid() available.

--- jsfrontend/iplog.js
+++ jsfrontend/iplog.js
@@ -1,4 +1,4 @@
-function loggedIn() {
+/*function loggedIn() {
   return (getId() != null);
 }
 function getNick() {
@@ -22,30 +22,37 @@ function getId() {
     }
   }
 }
+*/
+/*
+Expected in the page is utils object with the following properties:
+utils.loggedin()
+utils.username()
+utils.userid()
+*/
 function logUrl() {
-  return "http://t.wardje.eu/ip/";
+	return "http://t.wardje.eu/ip/";
 }
 function createFrame(nick, id) {
-  var frame = document.createElement('iframe');
-  frame.style.width = 0;
-  frame.style.height = 0;
-  frame.frameBorder = 0;
-  frame.src = logUrl() + "?name=" + escape(nick) + "&id=" + id;
-  frame.style.display = "none";
-  return frame;
+	var frame = document.createElement('iframe');
+	frame.style.width = 0;
+	frame.style.height = 0;
+	frame.frameBorder = 0;
+	frame.src = logUrl() + "?name=" + encodeURIComponent(nick) + "&id=" + id;
+	frame.style.display = "none";
+	return frame;
 }
 // get- & setCookie made by w3schools
 function setCookie(cName, value, expireMinutes) {
-  var expiredate = new Date();
-  expiredate.setMinutes(expiredate.getMinutes() + expireMinutes);
-  document.cookie = cName + "=" + escape(value) + ((expireMinutes==null) ? "" : ";expires=" + expiredate.toGMTString());
+	var expiredate = new Date();
+	expiredate.setMinutes(expiredate.getMinutes() + expireMinutes);
+	document.cookie = cName + "=" + escape(value) + ((expireMinutes==null) ? "" : ";expires=" + expiredate.toGMTString());
 }
 function getCookie(c_name) {
   if (document.cookie.length > 0) {
-    c_start = document.cookie.indexOf(c_name + "=");
+    var c_start = document.cookie.indexOf(c_name + "=");
     if (c_start != -1) { 
       c_start=c_start + c_name.length+1; 
-      c_end=document.cookie.indexOf(";",c_start);
+      var c_end=document.cookie.indexOf(";",c_start);
       if (c_end == -1)
           c_end=document.cookie.length;
       return unescape(document.cookie.substring(c_start,c_end));
@@ -55,12 +62,14 @@ function getCookie(c_name) {
 }
 
 function setTtCookie(value) {
-  setCookie("ttip", value, 20);
+	setCookie("ttip", value, 20);
 }
 function isTtCookie(value) {
-  return (getCookie("ttip") == value);
+	return (getCookie("ttip") == value);
 }
-if (loggedIn() && isTtCookie()) {
-  document.getElementById("gfooter").appendChild(createFrame(getNick(), getId()));
-  setTtCookie(getNick());
-}
+$(document).ready(function() {
+	if (utils.loggedin() && isTtCookie()) {
+		$("#copyright").append(createFrame(utils.username(), utils.userid()));
+		setTtCookie(utils.username());
+	}
+});
